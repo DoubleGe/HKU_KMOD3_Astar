578dcc4400a35975008ff4581be4cba1e93616c0
Wesley de Graaf <double.gezicht@gmail.com>
2024-11-19 16:04:09 +0100

    A* Pathfinding

--- Astar/Assets/Scripts/Astar.cs
+++ Astar/Assets/Scripts/Astar.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEditor.SceneManagement;
+using UnityEditor;
+using System.Linq.Expressions;
 
 public class Astar
 {
+    private const int MOVE_STRAIGHT_COST = 10;
+    private const int MOVE_DIAGONAL_COST = 14;
+
+    private List<Node> openList = new List<Node>();
+    private List<Cell> closedList = new List<Cell>();
+    private Dictionary<Vector2Int, Node> nodePositions;
+
     /// <summary>
     /// TODO: Implement this function so that it returns a list of Vector2Int positions which describes a path from the startPos to the endPos
     /// Note that you will probably need to add some helper functions
@@ -15,7 +25,85 @@ public class Astar
     /// <returns></returns>
     public List<Vector2Int> FindPathToTarget(Vector2Int startPos, Vector2Int endPos, Cell[,] grid)
     {
+        Cell startCell = grid[startPos.x, startPos.y];
+
+        Node startNode = new Node(startCell.gridPosition, null, startCell, 0, CalculateDistance(startPos, endPos));
+
+        openList = new List<Node> { startNode };
+        closedList = new List<Cell>();
+        nodePositions = new Dictionary<Vector2Int, Node>
+        {
+            { startPos, startNode }
+        };
+        //openList.Add(new Node(startCell));
+
+        while (openList.Count > 0)
+        {
+            Node currentNode = GetLowestFCostNode(openList);
+
+            if(currentNode.position == endPos) return GetPath(currentNode);
+
+            openList.Remove(currentNode);
+            closedList.Add(currentNode.cell);
+
+            foreach (Cell neighbour in currentNode.cell.GetNeighbours(grid))
+            {
+                if (closedList.Contains(neighbour)) continue;
+
+                if (neighbour.gridPosition.y > currentNode.position.y && neighbour.HasWall(Wall.DOWN)) continue;
+                if (neighbour.gridPosition.y < currentNode.position.y && neighbour.HasWall(Wall.UP)) continue;
+                if (neighbour.gridPosition.x > currentNode.position.x && neighbour.HasWall(Wall.LEFT)) continue;
+                if(neighbour.gridPosition.x < currentNode.position.x && neighbour.HasWall(Wall.RIGHT)) continue;   
+                
+                int newGCost = currentNode.GCost + CalculateDistance(currentNode.position, neighbour.gridPosition);
+
+                if (nodePositions.ContainsKey(neighbour.gridPosition))
+                {
+                    Node neighbourNode = nodePositions[neighbour.gridPosition];
+                    if(newGCost < neighbourNode.GCost)
+                    {
+                        neighbourNode.parent = currentNode;
+                        neighbourNode.GCost = newGCost;
+                        neighbourNode.HCost = CalculateDistance(neighbourNode.position, endPos);
+                    }
+                }
+                else
+                {
+                    Node neighbourNode = new Node(neighbour.gridPosition, currentNode, neighbour, newGCost, CalculateDistance(currentNode.position, neighbour.gridPosition));
+                    nodePositions.Add(neighbour.gridPosition, neighbourNode);
+                    openList.Add(neighbourNode);
+                }
+            }
+        }
+
         return null;
+    }
+
+    private List<Vector2Int> GetPath(Node endNode)
+    {
+        List<Vector2Int> path = new List<Vector2Int> { endNode.position };
+        Node currentNode = endNode;
+
+        while(currentNode.parent != null)
+        {
+            path.Add(currentNode.position);
+            currentNode = currentNode.parent;
+        }
+        path.Reverse();
+        return path;
+    }
+
+    private Node GetLowestFCostNode(List<Node> nodeList)
+    {
+        return nodeList.OrderBy(n => n.FScore).ToList()[0];
+    }
+
+    private int CalculateDistance(Vector2Int start, Vector2Int end)
+    {
+        int xDistance = Mathf.Abs(start.x - end.x);
+        int yDistance = Mathf.Abs(start.y - end.y);
+        int remaining = Mathf.Abs(xDistance - yDistance);
+        return MOVE_DIAGONAL_COST * Mathf.Min(xDistance, yDistance) + MOVE_STRAIGHT_COST * remaining;
     }
 
     /// <summary>
@@ -25,20 +113,22 @@ public class Astar
     {
         public Vector2Int position; //Position on the grid
         public Node parent; //Parent Node of this node
+        public Cell cell;
 
-        public float FScore { //GScore + HScore
-            get { return GScore + HScore; }
+        public int FScore { //GScore + HScore
+            get { return GCost + HCost; }
         }
-        public float GScore; //Current Travelled Distance
-        public float HScore; //Distance estimated based on Heuristic
+        public int GCost; //Current Travelled Distance
+        public int HCost; //Distance estimated based on Heuristic
 
         public Node() { }
-        public Node(Vector2Int position, Node parent, int GScore, int HScore)
+        public Node(Vector2Int position, Node parent, Cell cell, int GScore, int HScore)
         {
             this.position = position;
             this.parent = parent;
-            this.GScore = GScore;
-            this.HScore = HScore;
+            this.cell = cell;
+            this.GCost = GScore;
+            this.HCost = HScore;
         }
     }
 }
