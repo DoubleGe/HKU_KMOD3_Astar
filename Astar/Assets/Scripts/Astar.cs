52e7484e6f0241e2caa0d464317ee554c2d8e907
Wesley de Graaf <double.gezicht@gmail.com>
2024-11-19 16:58:03 +0100

    Added Heap

--- Astar/Assets/Scripts/Astar.cs
+++ Astar/Assets/Scripts/Astar.cs
@@ -11,8 +11,8 @@ public class Astar
     private const int MOVE_STRAIGHT_COST = 10;
     private const int MOVE_DIAGONAL_COST = 14;
 
-    private List<Node> openList = new List<Node>();
-    private List<Cell> closedList = new List<Cell>();
+    private Heap<Node> openList;
+    private List<Cell> closedList;
     private Dictionary<Vector2Int, Node> nodePositions;
 
     /// <summary>
@@ -29,13 +29,14 @@ public class Astar
 
         Node startNode = new Node(startCell.gridPosition, null, startCell, 0, CalculateDistance(startPos, endPos));
 
-        openList = new List<Node> { startNode };
+        openList = new Heap<Node>(grid.GetLength(0) * grid.GetLength(1));
+        openList.Add(startNode);
+
         closedList = new List<Cell>();
         nodePositions = new Dictionary<Vector2Int, Node>
         {
             { startPos, startNode }
         };
-        //openList.Add(new Node(startCell));
 
         while (openList.Count > 0)
         {
@@ -43,7 +44,6 @@ public class Astar
 
             if(currentNode.position == endPos) return GetPath(currentNode);
 
-            openList.Remove(currentNode);
             closedList.Add(currentNode.cell);
 
             foreach (Cell neighbour in currentNode.cell.GetNeighbours(grid))
@@ -93,9 +93,9 @@ public class Astar
         return path;
     }
 
-    private Node GetLowestFCostNode(List<Node> nodeList)
+    private Node GetLowestFCostNode(Heap<Node> nodeList)
     {
-        return nodeList.OrderBy(n => n.FScore).ToList()[0];
+        return nodeList.RemoveFirst();
     }
 
     private int CalculateDistance(Vector2Int start, Vector2Int end)
@@ -109,15 +109,19 @@ public class Astar
     /// <summary>
     /// This is the Node class you can use this class to store calculated FScores for the cells of the grid, you can leave this as it is
     /// </summary>
-    public class Node
+    public class Node : IHeapItem<Node>
     {
         public Vector2Int position; //Position on the grid
         public Node parent; //Parent Node of this node
         public Cell cell;
 
-        public int FScore { //GScore + HScore
+        public int FCost { //GScore + HScore
             get { return GCost + HCost; }
         }
+
+        private int heapIndex;
+        public int HeapIndex { get => heapIndex; set => heapIndex = value; }
+
         public int GCost; //Current Travelled Distance
         public int HCost; //Distance estimated based on Heuristic
 
@@ -129,6 +133,16 @@ public class Astar
             this.cell = cell;
             this.GCost = GScore;
             this.HCost = HScore;
+        }
+
+        public int CompareTo(Node nodeToCompare)
+        {
+            int compare = FCost.CompareTo(nodeToCompare.FCost);
+            if (compare == 0)
+            {
+                compare = HCost.CompareTo(nodeToCompare.HCost);
+            }
+            return -compare;
         }
     }
 }
